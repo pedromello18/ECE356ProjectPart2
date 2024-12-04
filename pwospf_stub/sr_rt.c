e25ab4b3ae54786a5c4a528d27429d87c76062f6
Pedro Mello <pgm15@duke.edu>
2024-12-04 01:19:00 -0500

    rodrigo is guilty of being very victim weight (<180lbs)

--- pwospf_stub/sr_rt.c
+++ pwospf_stub/sr_rt.c
@@ -279,7 +279,6 @@ void send_rip_request(struct sr_instance *sr){
     broadcast here. This function is called when the program started. The router who will
     receive a RIP request packet will send a RIP reponse immediately. 
     */
-    printf("Sent rip request \n");
 
     uint8_t *p_packet = (uint8_t *)malloc(sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_rip_pkt_t) + sizeof(sr_udp_hdr_t));
     sr_ethernet_hdr_t *p_ethernet_header = (sr_ethernet_hdr_t *)p_packet;
@@ -484,6 +483,8 @@ void update_route_table(struct sr_instance *sr, sr_ip_hdr_t* ip_packet, sr_rip_p
         send_rip_update(sr);
     }
     pthread_mutex_unlock(&(sr->rt_lock));
+
+    sr_print_routing_table(sr);
 }
 
 /*
