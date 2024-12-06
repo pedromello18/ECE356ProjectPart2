7d1452161738ec32901f12f6b056f182771c2b8d
Pedro Mello <pgm15@duke.edu>
2024-12-06 16:01:48 -0500

    htons(LDOC) = CODL

--- pwospf_stub/sr_arpcache.c
+++ pwospf_stub/sr_arpcache.c
@@ -29,6 +29,7 @@ void handle_arpreq(struct sr_instance *sr, struct sr_arpreq* req) {
             printf("hello from line 29 in arpcache.\n");
             char *iface_name = best_prefix(sr, req->ip);
             unsigned char mac_addr[ETHER_ADDR_LEN];
+            printf("line 32 so 33 is now actually 34\n");
             uint32_t ip_addr;
             printf("line 33\n");
             printf("F*ck u u piece of sh1t FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU*K");
