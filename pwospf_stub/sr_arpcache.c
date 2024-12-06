1df8fc704a16c1ced0f87b1b31eb4cb19806eab2
Pedro Mello <pgm15@duke.edu>
2024-12-06 15:42:22 -0500

    htons(LDOC) = CODL

--- pwospf_stub/sr_arpcache.c
+++ pwospf_stub/sr_arpcache.c
@@ -32,9 +32,10 @@ void handle_arpreq(struct sr_instance *sr, struct sr_arpreq* req) {
                 printf("iface_name is a null pointer. RIP! (no pun intended)\n");
                 return;
             }
-            printf("Interface:");
+            printf("Interface: ");
             printf(iface_name);
-            printf("\n ok i tried printing iface_name: %s\n", iface_name);
+            printf("\n ok i tried printing iface_name");
+            printf("iface_name: %s", iface_name);
             unsigned char mac_addr[ETHER_ADDR_LEN];
             uint32_t ip_addr;
 
