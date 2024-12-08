8265ba430cff0411c340c04ca2b7bd55b60b53c2
Pedro Mello <pgm15@duke.edu>
2024-12-08 17:15:31 -0500

    f

--- pwospf_stub/sr_rt.c
+++ pwospf_stub/sr_rt.c
@@ -289,7 +289,7 @@ void *sr_rip_timeout(void *sr_ptr) {
                 }
                 cur_rt = cur_rt->next;
             }
-            if (in_rt == 0) {
+            if ((in_rt == 0) || (cur_rt == NULL)) {
                 struct in_addr dest_ip;
                 dest_ip.s_addr = cur_if->ip;
                 struct in_addr gw_ip;
